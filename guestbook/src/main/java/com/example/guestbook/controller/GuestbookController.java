cdaa557e64872e9b85c05869bd7142dfe5b0b591
leaven153 <busmoja@naver.com>
2024-01-11 20:40:19 +0900

    240111 20:39 IntelliJ leaven153 게시글목록, 등록, 게시글 상세 조회

--- guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,18 +1,60 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
-    @GetMapping({"/", "/list"})
-    public String list(){
-        log.info("list..........");
-        return "/guestbook/list";
+
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
     }//public String list() ends
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list.........." + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }//public String list() ends
+
+    /******
+     * 게시글 등록을 누르면, 게시글을 입력할 수 있는 페이지로 이동한다
+     * *****/
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get............");
+    } // end of register()
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..." + dto);
+        Long gno = service.register(dto);
+        // addFlashAttribute는 단 한 번만 데이터를 전달하는 용도로 사용한다.
+        // redirectAttributes를 이용해서 한 번만 화면에서 "msg"라는 변수를 사용할 수 있도록 한다.
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    } // end of registerPost()
+
+    @GetMapping("/read")
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+        log.info("gno: " + gno);
+        GuestbookDTO dto = service.read(gno);
+        model.addAttribute("dto", dto);
+    } // end of read()
 
 } // public class GuestbookController ends
---
+++ guestbook/src/main/java/com/example/guestbook/dto/PageRequestDTO.java
@@ -0,0 +1,38 @@
+package com.example.guestbook.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class PageRequestDTO {
+    /***
+     * 페이지 번호 page
+     * 페이지 내 목록 개수 size
+     */
+    private int page;
+    private int size;
+
+    /***
+     * 페이지가 1부터 시작하고
+     * 페이지 내 10개 목록 출력되도록 설정
+     */
+    public PageRequestDTO(){
+        this.page = 1;
+        this.size = 10;
+    }
+    /****
+     * PageRequestDTO의 목적: JPA가 사용할 Pageable 객체 생성
+     * page-1: JPA는 페이지 번호가 0부터 시작하므로, 1페이지의 경우 0이 되도록 page-1로 처리 (추후에 수정 필요한 상황 발생할 수 있음)
+     * sort: 정렬의 형태는 한정짓지 않기 위해 parameter로 받음
+     * **/
+    public Pageable getPageable(Sort sort){
+        return PageRequest.of(page-1, size, sort);
+    }// end of getPageable
+
+} // end of class PageRequestDTO
